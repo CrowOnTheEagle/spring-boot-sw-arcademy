8a93e997bec883fba9f06f7f7607ea49d362fcdb
CrowOnTheEagle <blizkrig553@gmail.com>
2025-08-01 16:51:46 +0900

    20250801 end

--- src/main/java/com/arcademy/spring_boot_sw_arcademy/SpringBootSwArcademyApplication.java
+++ src/main/java/com/arcademy/spring_boot_sw_arcademy/SpringBootSwArcademyApplication.java
@@ -6,18 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+// 웹 브라우저에 리턴된 문자열을 출력
+// Print any of the returned string into the web browser
 @RestController
 public class SpringBootSwArcademyApplication {
-	// 시작 페이지 진입 시 이 함수 호출
-	// This function will be called when the user enters the start page
+	// @GetMapping에서 지정한 경로에 접근 시 함수 호출함
+	// The function will be called when the user access specific URL/path
+	// defined by the @GetMapping annotation.
 	@GetMapping("/")
+	public String PrintMain() {
+		return "이 페이지는 메인 홈페이지 입니다.\nThis page is Main page";
+	}
+
+	@GetMapping("/apple")
 	public String Hello() {
-		return "NOTHING HERE\n";
+		return "BIG DATA\n빅데이터\n";
 	}
 
 	public static void main(String[] args) {
 		System.out.println("SPRING STARTED!!!");
 		SpringApplication.run(SpringBootSwArcademyApplication.class, args);
 	}
-
 }
