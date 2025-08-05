112f7968771f29d111dc74970aa1c4f74fce74e3
CrowOnTheEagle <blizkrig553@gmail.com>
2025-08-05 16:50:30 +0900

    Squashed commit of the following:
    commit cc2e81e18ee1ff00767d4ba595a2a19ff91b0e73
    Author: CrowOnTheEagle <blizkrig553@gmail.com>
    Date:   Tue Aug 5 16:46:28 2025 +0900
    
        20250805; add git actions

--- src/main/java/com/arcademy/spring_boot_sw_arcademy/SpringBootSwArcademyApplication.java
+++ src/main/java/com/arcademy/spring_boot_sw_arcademy/SpringBootSwArcademyApplication.java
@@ -1,35 +1,14 @@
 package com.arcademy.spring_boot_sw_arcademy;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
-// 웹 브라우저에 리턴된 문자열을 출력
-// Print any of the returned string into the web browser
 @RestController
 public class SpringBootSwArcademyApplication {
-	// @GetMapping에서 지정한 경로에 접근 시 함수 호출함
-	// The function will be called when the user access specific URL/path
-	// defined by the @GetMapping annotation.
-	@GetMapping("/Time")
+	@GetMapping("/")
 	public String MainPage() {
-		return "20250804-171508-test01\n";
+		return "Spring02:20250805-145002\n";
 	}
-	
-	@GetMapping("/Main")
-	public String PrintMain() {
-		return "이 페이지는 메인 홈페이지 입니다.\nThis page is Main page";
-	}
-
-	@GetMapping("/apple")
-	public String Hello() {
-		return "BIG DATA\n빅데이터\n";
-	}
-
 	public static void main(String[] args) {
 		System.out.println("SPRING STARTED!!!");
 		SpringApplication.run(SpringBootSwArcademyApplication.class, args);
---
+++ src/main/java/com/arcademy/spring_boot_sw_arcademy/SpringBootSwArcademyApplication_OLD.java
@@ -0,0 +1,32 @@
+package com.arcademy.spring_boot_sw_arcademy;
+import org.springframework.boot.*;
+//import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+//@SpringBootApplication
+// 웹 브라우저에 리턴된 문자열을 출력
+// Print any of the returned string into the web browser
+//@RestController
+public class SpringBootSwArcademyApplication_OLD {
+	// @GetMapping에서 지정한 경로에 접근 시 함수 호출함
+	// The function will be called when the user access specific URL/path
+	// defined by the @GetMapping annotation.
+	@GetMapping("/")
+	public String MainPage() {
+		return "Spring02:20250805-145002\n";
+	}
+	
+	@GetMapping("/Main")
+	public String PrintMain() {
+		return "이 페이지는 메인 홈페이지 입니다.\nThis page is Main page";
+	}
+
+	@GetMapping("/apple")
+	public String Hello() {
+		return "BIG DATA\n빅데이터\n";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("SPRING STARTED!!!");
+		SpringApplication.run(SpringBootSwArcademyApplication.class, args);
+	}
+}
