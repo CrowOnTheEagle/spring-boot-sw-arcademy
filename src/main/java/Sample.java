563f9b00450bd2f6a7ad35b3452b27242b5eb359
CrowOnTheEagle <blizkrig553@gmail.com>
2025-08-05 17:17:34 +0900

    20250805; add java sample file

---
+++ src/main/java/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("NETKELWNNKLF NWEFN#I) NFIOQ#N OQNF)# NKLANS ");
+        System.out.println("сука блять");
+        System.out.println(1234);
+        System.out.println(Long.MAX_VALUE);
+    }
+}
